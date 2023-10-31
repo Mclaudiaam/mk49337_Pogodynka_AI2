6100d08928b11484f3268844e4b38b87bb976941
mk49337 <mk49337@zut.edu.pl>
2023-10-31 15:07:41 +0100

    lab-d

---
+++ src/Form/LocationType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Location;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('city', null, [
+                'attr' => [
+                    'placeholder' => 'Enter city name',
+                ],
+            ])
+            ->add('country', ChoiceType::class, [
+                'choices' => [
+                    'Poland' => 'PL',
+                    'German' => 'DE',
+                    'France' => 'FR',
+                    'Spain' => 'ES',
+                    'Italy' => 'IT',
+                ]
+            ])
+            ->add('latitude')
+            ->add('longitude')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+
+//    public function __toString() {
+//        return (string) $this->city;
+//    }
+}
